5e85f953c8963193e880191f171b0fc398c7f8d4
Ilgiz Farrakhov <farrakhov.frontend@gmail.com>
2023-07-30 14:00:57 +0300

    changed the selector of the selected pagination button

--- src/components/Pagination/styled-components.js
+++ src/components/Pagination/styled-components.js
@@ -48,7 +48,7 @@ export const Paginate = styled(PagComponent)`
     }
 
     //selected
-    & .css-yuzg60-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected{
+    & .css-1xr9krm.Mui-selected{
         background-color: var(--bg-inverse);
         color: var(--color-text-inverse);
 
